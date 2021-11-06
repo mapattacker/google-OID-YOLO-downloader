aacadc21442b0ea77e5062d342bccc0aa5a7d47e
Jake <mapattacker@gmail.com>
2021-11-06 11:58:30 +0800

    create metadata folder

--- download_metadata.sh
+++ download_metadata.sh
@@ -1,3 +1,6 @@
+mkdir metadata
+cd metadata
+
 # download train, val, test bboxes
 wget https://storage.googleapis.com/openimages/v6/oidv6-train-annotations-bbox.csv
 wget https://storage.googleapis.com/openimages/v5/validation-annotations-bbox.csv
